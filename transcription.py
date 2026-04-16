2ce28a5a12c2d678f73619f4361a10e76a41c281
Linkado36778 <barbosaa_lucas@hotmail.com>
2026-04-16 20:05:36 -0300

    refactor: optimizing usage of the function transcrever_comando

--- transcription.py
+++ transcription.py
@@ -1,7 +1,10 @@
 import whisper
 from soundMic import gravar_por_tecla
 
-model = whisper.load_model("base")
-gravacao = gravar_por_tecla()
-result = model.transcribe(gravacao, language="en", fp16=False)
-print(result["text"])
+def transcrever_comando():
+    model = whisper.load_model("base")
+    gravacao = gravar_por_tecla()
+    result = model.transcribe(gravacao, language="en", fp16=False)
+    print(result["text"])
+
+    return result["text"]
